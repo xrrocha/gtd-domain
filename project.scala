aac56ae5e3b5631dc70cf43e04a4f4ab38d9d178
Ricardo Rocha <xrrocha@gmail.com>
2024-02-17 16:14:14 -0500

    Separated resource directories for tests

--- project.scala
+++ project.scala
@@ -1,8 +1,10 @@
 //> using javaOpt -Xmx4g
 //> using scala 3.3.1
 //> using options -deprecation -release:21
-//> using resourceDirs ./
+//> using resourceDirs ./resources
+//> using test.resourceDirs ./test-resources
 
 //> using dep org.scala-lang.modules::scala-parallel-collections::1.0.4
 
 //> using test.dep org.scalameta::munit::1.0.0-M7
+
