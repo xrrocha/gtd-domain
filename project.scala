c5e1c57241ba761d2d46c75a597604175b27595a
Ricardo Rocha <xrrocha@gmail.com>
2024-02-17 08:46:18 -0500

    Inital commit

---
+++ project.scala
@@ -0,0 +1,8 @@
+//> using javaOpt -Xmx4g
+//> using scala 3.3.1
+//> using options -deprecation -release:21
+//> using resourceDirs ./
+
+//> using dep org.scala-lang.modules::scala-parallel-collections::1.0.4
+
+//> using test.dep org.scalameta::munit::1.0.0-M7
